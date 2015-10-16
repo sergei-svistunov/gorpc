39ad5085db81e317e31a549202696f096638fb69
Andrei Korzhevskii <andrei.korzhevskii@lazada.com>
2015-10-16 19:18:40 +0300

    Print debug info for first level of request params

--- handler_info.go
+++ handler_info.go
@@ -16,13 +16,13 @@ func (hi *handlerInfo) String() string {
 		fmt.Sprintf("\tCaption: %s\n\tDescription:\n\t\t%s\n\tVersions:\n",
 			hi.Caption,
 			strings.Replace(hi.Description, "\n", "\n\t\t", -1))
-		//	for vN, version := range hi.Versions {
-		//		res += fmt.Sprintf("\t\t%d:\n\t\t\tParameters:\n", vN+1)
-		//		for _, parameter := range version.Parameters {
-		//			res += fmt.Sprintf("\t\t\t\t%s:\n\t\t\t\t\tType: %s\n\t\t\t\t\tDescription: %s\n\t\t\t\t\tIs required: %t\n",
-		//				parameter.Name, parameter.RawType.Kind().String(), parameter.Description, parameter.IsRequired)
-		//		}
-		//	}
+	for vN, version := range hi.Versions {
+		res += fmt.Sprintf("\t\t%d:\n\t\t\tParameters:\n", vN+1)
+		for _, parameter := range version.Request.Fields {
+			res += fmt.Sprintf("\t\t\t\t%s:\n\t\t\t\t\tType: %s\n\t\t\t\t\tDescription: %s\n\t\t\t\t\tIs required: %t\n",
+				parameter.Name, parameter.RawType.Kind().String(), parameter.Description, parameter.IsRequired)
+		}
+	}
 
 	return res
 }
