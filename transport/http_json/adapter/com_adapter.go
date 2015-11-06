f8cf2fea308b0e4a6c6f920e2e25c231b446a47d
Yuriy Savalin <yuriy.savalin@lazada.com>
2015-11-06 19:58:02 +0300

    [Issue #10] Remove GET methods and related code

--- transport/http_json/adapter/com_adapter.go
+++ transport/http_json/adapter/com_adapter.go
@@ -3,7 +3,6 @@ package adapter
 import (
 	"bytes"
 	"fmt"
-	"github.com/sergei-svistunov/gorpc"
 	"regexp"
 	"strings"
 )
@@ -29,17 +28,6 @@ var mainImports = []string{
 	"bytes",
 	"io/ioutil",
 }
-
-var handlerCallGetFuncTemplate = []byte(`
-func (api *>>>API_NAME<<<) >>>HANDLER_NAME<<<(ctx context.Context>>>INPUT_PARAMS<<<) (>>>RETURNED_TYPE<<<, error) {
-    var result >>>RETURNED_TYPE<<<
-    params := map[string]interface{}{>>>MAPPED_INPUT_PARAMS<<<}
-
-    err := api.get(ctx, ">>>HANDLER_PATH<<<", params, &result)
-	return result, err
-}
-
-`)
 
 var staticLogicTemplate = `
 type httpSessionResponse struct {
@@ -76,39 +64,6 @@ func New>>>API_NAME<<<(balancer IBalancer, apiTimeout int, serviceName string) *
 		ServiceName:   serviceName,
 		balancer: balancer,
 	}
-}
-
-func (api *>>>API_NAME<<<) get(ctx context.Context, path string, params map[string]interface{}, buf interface{}) error {
-	values := toURLValues(params)
-	return api.getWithValues(ctx, path, values, buf)
-}
-
-func (api *>>>API_NAME<<<) getWithValues(ctx context.Context, path string, values url.Values, buf interface{}) error {
-    apiURL, err := api.balancer.Next()
-    if err != nil {
-        return err
-    }
-    sessionRequest := &sessionRequest{
-        Path: path,
-        Params: values,
-        URL: apiURL,
-    }
-	return api.call(ctx, sessionRequest, func(serviceURL string) (interface{}, error) {
-		r, err := http.NewRequest("GET", createRawURL(serviceURL, path, values), nil)
-		if err != nil {
-			return nil, err
-		}
-
-		wrapper := httpSessionResponse{}
-		if err := do(api.client, r, &wrapper); err != nil {
-			return nil, err
-		}
-        if err := json.Unmarshal(wrapper.Data, buf); err != nil {
-            return nil, err
-        }
-
-		return buf, nil
-	})
 }
 
 func (api *>>>API_NAME<<<) set(ctx context.Context, path string, data interface{}, buf interface{}) error {
@@ -155,17 +110,6 @@ func (api *>>>API_NAME<<<) call(ctx context.Context, sessionRequest *sessionRequ
     }
 
     return
-}
-
-func toURLValues(params map[string]interface{}) url.Values {
-	var values url.Values
-	if len(params) > 0 {
-		values = url.Values{}
-		for k, v := range params {
-			values.Set(k, fmt.Sprintf("%v", v))
-		}
-	}
-	return values
 }
 
 func createRawURL(url, path string, values url.Values) string {
@@ -217,45 +161,7 @@ func do(client *http.Client, request *http.Request, buf interface{}) (err error)
 	return nil
 }
 
-
 `
-
-func replaceServiceName(codePtr *[]byte) error {
-	if codePtr == nil {
-		return fmt.Errorf("Code pointer is nil")
-	}
-
-	*codePtr = regexp.MustCompilePOSIX(">>>SERVICE_NAME<<<").ReplaceAll(*codePtr, []byte(strings.ToLower(serviceName)))
-	*codePtr = regexp.MustCompilePOSIX(">>>API_NAME<<<").ReplaceAll(*codePtr, []byte(strings.Title(serviceName)))
-	handlerCallGetFuncTemplate = regexp.MustCompilePOSIX(">>>API_NAME<<<").ReplaceAll(handlerCallGetFuncTemplate, []byte(strings.Title(serviceName)))
-	handlerCallPostFuncTemplate = regexp.MustCompilePOSIX(">>>API_NAME<<<").ReplaceAll(handlerCallPostFuncTemplate, []byte(strings.Title(serviceName)))
-	return nil
-}
-
-func generateAdapterMethods(structsBuf *bytes.Buffer) []byte {
-	result := &bytes.Buffer{}
-
-	for path, handlerInfo := range path2HandlerInfoMapping {
-		var method []byte
-		/*
-			method = regexp.MustCompilePOSIX(">>>HANDLER_PATH<<<").ReplaceAll(handlerCallGetFuncTemplate, []byte(path))
-			path = strings.Replace(strings.Title(path), "/", "", -1) //TODO convert to CamelCaseName
-			method = regexp.MustCompilePOSIX(">>>HANDLER_NAME<<<").ReplaceAll(method, []byte(path))
-			method = regexp.MustCompilePOSIX(">>>RETURNED_TYPE<<<").ReplaceAll(method, []byte(handlerInfo.Output))
-			method = regexp.MustCompilePOSIX(">>>INPUT_PARAMS<<<").ReplaceAll(method, generateInputParamsRow(handlerInfo.Params, structsBuf))
-			method = regexp.MustCompilePOSIX(">>>MAPPED_INPUT_PARAMS<<<").ReplaceAll(method, generateMappedInputParamsString(handlerInfo.Params))
-		*/
-		method = regexp.MustCompilePOSIX(">>>HANDLER_PATH<<<").ReplaceAll(handlerCallPostFuncTemplate, []byte(path))
-		path = strings.Replace(strings.Title(path), "/", "", -1) //TODO convert to CamelCaseName
-		method = regexp.MustCompilePOSIX(">>>HANDLER_NAME<<<").ReplaceAll(method, []byte(path))
-		method = regexp.MustCompilePOSIX(">>>INPUT_TYPE<<<").ReplaceAll(method, []byte(handlerInfo.Input))
-		method = regexp.MustCompilePOSIX(">>>RETURNED_TYPE<<<").ReplaceAll(method, []byte(handlerInfo.Output))
-
-		result.Write(method)
-	}
-
-	return result.Bytes()
-}
 
 var handlerCallPostFuncTemplate = []byte(`
 func (api *>>>API_NAME<<<) >>>HANDLER_NAME<<<(ctx context.Context, data >>>INPUT_TYPE<<<) (>>>RETURNED_TYPE<<<, error) {
@@ -267,33 +173,30 @@ func (api *>>>API_NAME<<<) >>>HANDLER_NAME<<<(ctx context.Context, data >>>INPUT
 
 `)
 
-func generateInputParamsRow(params []gorpc.HandlerParameter, additionalStructsBuf *bytes.Buffer) []byte {
-	var s string
-	for _, param := range params {
-		if param.RawType == nil {
-			// TODO log error?
-			//log.Errorf("Param %#v is nil", param)
-			continue
-		}
-		t, extraStructs := detectTypeName(param.RawType, nil)
-		s += (", " + param.Name + " " + t)
-		if len(extraStructs) > 0 {
-			for i := range extraStructs {
-				_, err := convertStructToCode(extraStructs[i], additionalStructsBuf, nil)
-				if err != nil {
-					// TODO log error?
-					panic(err)
-				}
-			}
-		}
+func replaceServiceName(codePtr *[]byte) error {
+	if codePtr == nil {
+		return fmt.Errorf("Code pointer is nil")
 	}
-	return []byte(s)
+
+	*codePtr = regexp.MustCompilePOSIX(">>>SERVICE_NAME<<<").ReplaceAll(*codePtr, []byte(strings.ToLower(serviceName)))
+	*codePtr = regexp.MustCompilePOSIX(">>>API_NAME<<<").ReplaceAll(*codePtr, []byte(strings.Title(serviceName)))
+	handlerCallPostFuncTemplate = regexp.MustCompilePOSIX(">>>API_NAME<<<").ReplaceAll(handlerCallPostFuncTemplate, []byte(strings.Title(serviceName)))
+	return nil
 }
 
-func generateMappedInputParamsString(params []gorpc.HandlerParameter) []byte {
-	var s string
-	for _, param := range params {
-		s += ("\"" + param.GetKey() + "\": " + param.Name + ",\n")
+func generateAdapterMethods(structsBuf *bytes.Buffer) []byte {
+	result := &bytes.Buffer{}
+
+	for path, handlerInfo := range path2HandlerInfoMapping {
+		var method []byte
+		method = regexp.MustCompilePOSIX(">>>HANDLER_PATH<<<").ReplaceAll(handlerCallPostFuncTemplate, []byte(path))
+		path = strings.Replace(strings.Title(path), "/", "", -1) //TODO convert to CamelCaseName
+		method = regexp.MustCompilePOSIX(">>>HANDLER_NAME<<<").ReplaceAll(method, []byte(path))
+		method = regexp.MustCompilePOSIX(">>>INPUT_TYPE<<<").ReplaceAll(method, []byte(handlerInfo.Input))
+		method = regexp.MustCompilePOSIX(">>>RETURNED_TYPE<<<").ReplaceAll(method, []byte(handlerInfo.Output))
+
+		result.Write(method)
 	}
-	return []byte(s)
+
+	return result.Bytes()
 }
